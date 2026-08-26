d902f4e71c8b2f5186e29b9e343942dbcfcce08f
praveen1242 <praveen1242@gmail.com>
2026-08-26 10:13:51 +0530

    configure test database for CI

--- backend/src/test/java/com/social/platform/SocialPlatformApplicationTests.java
+++ backend/src/test/java/com/social/platform/SocialPlatformApplicationTests.java
@@ -2,12 +2,13 @@ package com.social.platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SocialPlatformApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
